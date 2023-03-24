5c64e082b462c88a10834217720637eacd62c0b5
n-osborne <nicolas.osborne@tarides.com>
2023-03-24 10:39:38 +0100

    Use result type instead of option
    Add a variant type expressing kind of errors we can encounter while
    building the intermediate representation. We don't stop at the first
    error we cross, getting the user more information.

--- plugins/qcheck-stm/src/ir_of_gospel.ml
+++ plugins/qcheck-stm/src/ir_of_gospel.ml
@@ -4,6 +4,20 @@ open Config
 open Ir
 module Ident = Identifier.Ident
 
+type failure =
+  | Init_is_a_constant
+  | Init_has_multiple_sut_arguments
+  | Init_has_no_sut_argument
+  | Init_return_sut
+
+let flatten =
+  let rec aux acc = function
+    | [] -> acc
+    | Error err :: xs -> aux (err :: acc) xs
+    | _ :: xs -> aux acc xs
+  in
+  aux []
+
 let lb_arg_is_of_type ts = function
   | Lunit -> false
   | Lnone vs | Loptional vs | Lnamed vs | Lghost vs -> (
@@ -14,30 +28,37 @@ let lb_arg_is_of_type ts = function
 let lb_arg_is_not_of_type ty lb_arg = not (lb_arg_is_of_type ty lb_arg)
 
 let rec exactly_one p = function
-  | [] -> false
-  | x :: xs when p x -> List.exists p xs |> not
+  | [] -> Error Init_has_no_sut_argument
+  | x :: xs when p x ->
+      if List.exists p xs then Error Init_has_multiple_sut_arguments else Ok ()
   | _ :: xs -> exactly_one p xs
 
 let is_stmable config vd =
-  (* the value is not a constant *)
-  match vd.vd_args with
-  | [] -> false
-  | args ->
-      (* there is exactly one argument of the type sut *)
-      exactly_one (lb_arg_is_of_type config.sut) args
-      (* the function does not return a value of the type sut *)
-      && List.for_all (lb_arg_is_not_of_type config.sut) vd.vd_ret
+  let test_constant =
+    match vd.vd_args with [] -> Error Init_is_a_constant | _ -> Ok ()
+  in
+  let test_return =
+    if List.for_all (lb_arg_is_not_of_type config.sut) vd.vd_ret then Ok ()
+    else Error Init_return_sut
+  in
+  flatten
+    [
+      test_constant;
+      exactly_one (lb_arg_is_of_type config.sut) vd.vd_args;
+      test_return;
+    ]
 
 let val_desc config vd =
-  if is_stmable config vd then
-    let id = vd.vd_name.id_str in
-    let ty = vd.vd_type in
-    Some { id; ty }
-  else None
+  match is_stmable config vd with
+  | [] ->
+      let id = vd.vd_name.id_str in
+      let ty = vd.vd_type in
+      Ok { id; ty }
+  | failures -> Error failures
 
 let sig_item config s =
   match s.sig_desc with
-  | Sig_val (vd, Nonghost) -> val_desc config vd
+  | Sig_val (vd, Nonghost) -> Some (val_desc config vd)
   | _ -> None
 
 let signature config = List.filter_map (sig_item config)
--- plugins/qcheck-stm/test/expect_ir.ml
+++ plugins/qcheck-stm/test/expect_ir.ml
@@ -2,15 +2,18 @@ open Ortac_qcheck_stm
 
 let () =
   let go v =
-    let open Ir in
-    let str =
-      "Value "
-      ^ v.id
-      ^ " of type "
-      ^ Ppxlib.string_of_core_type v.ty
-      ^ " will be tested."
-    in
-    print_endline str
+    match v with
+    | Ok v ->
+        let open Ir in
+        let str =
+          "Value "
+          ^ v.id
+          ^ " of type "
+          ^ Ppxlib.string_of_core_type v.ty
+          ^ " will be tested."
+        in
+        print_endline str
+    | Error _ -> ()
   in
   match Config.init "lib.mli" "good_init" "sut" with
   | Error err -> print_endline err
