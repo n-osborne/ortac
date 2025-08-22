fa803d81d38a62e29d96be0c5be543819a9fedbf
Nicolas Osborne <nicolas.osborne@tarides.com>
2025-08-22 15:41:38 +0200

    Add gospel specifications to treiber stack interface
    Only the functions that are tested by the original STM tests are
    specified to be included in the generated tests and have equivalent
    testing coverage.
    
    Note that we can't specify to_seq in Gospel.

--- plugins/qcheck-stm/experimentation/treiber_stack.mli
+++ plugins/qcheck-stm/experimentation/treiber_stack.mli
@@ -5,11 +5,17 @@
 
 (** {1 API} *)
 
+(*@ predicate seq_is_empty (s : 'a sequence) =
+       s = Sequence.empty *)
+
 type 'a t
 (** Represents a lock-free Treiber stack holding elements of type ['a]. *)
+(*@ mutable model contents : 'a sequence *)
 
 val create : unit -> 'a t
 (** [create ()] creates a new empty Treiber stack. *)
+(*@ a = create ()
+    ensures a.contents = Sequence.empty *)
 
 val of_list : 'a list -> 'a t
 (** [of_list list] creates a new Treiber stack from a list. *)
@@ -17,6 +23,8 @@ val of_list : 'a list -> 'a t
 val is_empty : 'a t -> bool
 (** [is_empty stack] returns [true] if the [stack] is empty, otherwise [false].
 *)
+(*@ b = is_empty a
+    ensures b = seq_is_empty a.contents *)
 
 (** {2 Consumer functions} *)
 
@@ -32,6 +40,10 @@ val peek_exn : 'a t -> 'a
 val peek_opt : 'a t -> 'a option
 (** [peek_opt stack] returns [Some] of the top element of the [stack] without
     removing it, or [None] if the [stack] is empty. *)
+(*@ o = peek_opt a
+    ensures o = if seq_is_empty a.contents
+                then None
+                else Some (Sequence.hd a.contents) *)
 
 val pop_exn : 'a t -> 'a
 (** [pop_exn stack] removes and returns the top element of the [stack].
@@ -41,6 +53,14 @@ val pop_exn : 'a t -> 'a
 val pop_opt : 'a t -> 'a option
 (** [pop_opt stack] removes and returns [Some] of the top element of the
     [stack], or [None] if the [stack] is empty. *)
+(*@ o = pop_opt a
+    modifies a
+    ensures a.contents = if seq_is_empty (old a.contents)
+                         then Sequence.empty
+                         else Sequence.tl (old a.contents)
+    ensures o = if seq_is_empty (old a.contents)
+                then None
+                else Some (Sequence.hd (old a.contents)) *)
 
 val drop_exn : 'a t -> unit
 (** [drop_exn stack] removes the top element of the [stack].
@@ -64,11 +84,18 @@ val pop_all : 'a t -> 'a list
       # pop_all t
       - : int list = [3; 2; 1]
     ]} *)
+(*@ xs = pop_all a
+    modifies a.contents
+    ensures a.contents = Sequence.empty
+    ensures xs = List.of_seq (old a.contents) *)
 
 (** {2 Producer functions} *)
 
 val push : 'a t -> 'a -> unit
 (** [push stack element] adds [element] to the top of the [stack]. *)
+(*@ push a x
+    modifies a.contents
+    ensures a.contents = Sequence.cons x (old a.contents) *)
 
 val push_all : 'a t -> 'a list -> unit
 (** [push_all stack elements] adds all [elements] to the top of the [stack].
@@ -87,6 +114,9 @@ val push_all : 'a t -> 'a list -> unit
       # pop_all t
       - : int list = [2; 1]
     ]} *)
+(*@ push_all a xs
+    modifies a.contents
+    ensures a.contents = Sequence.rev (List.to_seq xs) ++ old a.contents *)
 
 (** {2 With Sequences}*)
 val to_seq : 'a t -> 'a Seq.t
